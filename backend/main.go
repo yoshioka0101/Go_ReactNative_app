e6f1abfbd52a02ee98c91754ce1b049cfd77f71f
yoshioka0101 <yuuki0205nano@gmail.com>
2025-01-28 14:42:51 +0900

    feat:Ginの初期設定

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	// "fmt"
+	//"net/http"
+	
+	"github.com/gin-gonic/gin"
+)
 
-func main(){
-	fmt.Println("Hello World")
+func main() {
+    r := gin.Default()
+    r.GET("/", func(c *gin.Context) {
+        c.JSON(200, gin.H{"message": "Hello World\n"})
+    })
+    r.Run(":8080")
 }
