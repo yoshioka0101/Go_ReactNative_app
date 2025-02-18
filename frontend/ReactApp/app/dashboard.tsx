df07ceb5a3568c52056cc3a7fd200e4ed338e2f2
yoshioka0101 <yuuki0205nano@gmail.com>
2025-02-18 19:08:45 +0900

    feat:ログインボタンの実装

--- frontend/ReactApp/app/dashboard.tsx
+++ frontend/ReactApp/app/dashboard.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { View, Text, ActivityIndicator } from 'react-native';
+import { View, Text, ActivityIndicator, Button, StyleSheet } from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
 
 export default function Dashboard() {
@@ -13,6 +13,13 @@ export default function Dashboard() {
     }
   }, [params]);
 
+  const handleLogout = () => {
+    localStorage.removeItem('accessToken');
+    setToken(null);
+    alert("ログアウトしました！！！！");
+    window.location.href = "/";
+  }
+
   if (!token) {
     return <ActivityIndicator />;
   }
@@ -20,7 +27,15 @@ export default function Dashboard() {
   return (
     <View>
       <Text>ログイン成功！</Text>
-      <Text>アクセストークン: {token}</Text>
+      <View style={styles.buttonContainer}>
+        <Button title = "ログアウト" onPress={handleLogout}></Button>
+      </View>
     </View>
   );
 }
+const styles = StyleSheet.create({
+    buttonContainer: {
+     marginTop: 16,
+     alignItems: 'center',
+    },
+});
